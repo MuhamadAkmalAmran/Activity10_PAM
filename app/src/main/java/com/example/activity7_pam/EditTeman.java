0027711dd7a5a04b20a3361d513391d1cf806987
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-05-25 19:21:18 +0700

    update ACT10

--- app/src/main/java/com/example/activity7_pam/EditTeman.java
+++ app/src/main/java/com/example/activity7_pam/EditTeman.java
@@ -33,7 +33,7 @@ public class EditTeman extends AppCompatActivity {
     String nm, tlp, id, namaEd, telponEd;
     int sukses;
 
-    private static String url_update = "http://10.0.2.2/umyTI/updatetm.php";
+    private static String url_update = "https://20200140138.praktikumtiumy.com/updatetm.php";
     public static final String TAG = EditTeman.class.getSimpleName();
     public static final String TAG_SUCCES = "success";
 
